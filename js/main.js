d76279c4bbc212ae3835d1aabb2772842198c92d
tu-nguyen <valcrune@gmail.com>
2021-02-20 10:18:51 -0500

    reworked portfolio

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    let $btns = $('.projects .button-group button');
+
+    $btns.click(function(e){
+        $('.projects .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.projects .projects-gallery').isotope({
+            filter:selector
+        });
+        return false;
+    })
+
+
+    $('.projects .projects-gallery .test-popup-link').magnificPopup({
+        type:'image',
+        gallery:{enabled:true}
+    });
+
+    // sticky navbar
+    window.onscroll = function() {myFunction()};
+    
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+});
