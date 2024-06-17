c752b4b7a47760786a40ec8cf1aa39e591d1a319
Tu Nguyen <valcrune@gmail.com>
2024-06-16 23:11:37 -0400

    revert app and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 
-const App = () => {
+function App() {
   return (
     <>
       <div>
