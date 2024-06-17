3d3cd39e17d4f9b734c04508b2cf033d23c22d8a
Tu Nguyen <valcrune@gmail.com>
2024-06-16 22:41:50 -0400

    attempt to revert some json settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <section>
-      <div className='grid h-screen place-items-center'>
-        <div className='mx-auto text-white text-center'>
-          <h1 className='text-5xl'>Tu Nguyen</h1>
-          <h3 className='text-4xl'>Full Stack Software Engineer</h3>
-          <h4 className='text-2xl'>Portfolio under construction (Updating to use <strong>React</strong> + <strong>Vite</strong> + <strong>Tailwind</strong>) <br /><a href="./v1/index.html"><strong>click here</strong></a> to view old version</h4>
+      <div>
+        <div className='grid h-screen place-items-center'>
+          <div className='mx-auto text-white text-center'>
+            <h1 className='text-5xl'>Tu Nguyen</h1>
+            <h3 className='text-4xl'>Full Stack Software Engineer</h3>
+            <h4 className='text-2xl'>Portfolio under construction (Updating to use <strong>React</strong> + <strong>Vite</strong> + <strong>Tailwind</strong>) <br /><a href="./v1/index.html"><strong>click here</strong></a> to view old version</h4>
+          </div>
         </div>
       </div>
-      </section>
     </>
   )
 }
