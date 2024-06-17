61e350b3d103e201b77e30a57cbdf2c76da75f5b
Tu Nguyen <valcrune@gmail.com>
2024-06-16 21:10:34 -0400

    slight change in base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "https://tu-nguyen.github.io/",
   plugins: [react()],
   server: {
     host: true,
