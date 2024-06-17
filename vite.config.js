09d6e9af17e8a6982ba82b20776330de9cb5aa36
Tu Nguyen <valcrune@gmail.com>
2024-06-17 09:08:11 -0400

    add vite static copy plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy  } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy ({
+      targets: [
+        {
+          src: 'v1',
+          dest: 'v1'
+        }
+      ]
+    })
+  ],
   base: "./",
   build: {
     outDir: "dist",
