eb14f8fce3968fb3408984f6a2ed68e71d37fe3a
Tu Nguyen <valcrune@gmail.com>
2024-06-21 14:57:49 -0400

    start basic footer and layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a>Designed in Figma and coded in Visual Studio Code by Tu Nguyen. Built with React.js, Vite and Tailwind CSS, deployed with GitHub Pages.</a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
