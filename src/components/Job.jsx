38236267fb78fa0478a375168356f597a893170e
Tu Nguyen <valcrune@gmail.com>
2024-06-21 21:30:45 -0400

    populate job in order and proper swap

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -10,33 +10,52 @@ const Job = ({ job }) => {
     description = description.substring(0, 90) + '...';
   }
 
-  return (
-    <div className="bg-white rounded-xl shadow-md relative">
-        <div className="p-4">
-            <div className="mb-6">
-                <div className="text-gray-600 my-2">
-                    {job.type}
-                </div>
-                <h3 className="text-xl font-bold">
-                    {job.title}
-                </h3>
-            </div>
-    
+  function JobItemA({title, company, alignment = '' }) {
+    return (
+        <div className={`mb-4 ${alignment}`}>
+            <h2 className="text-xl font-bold">
+                {title} - {company} 
+            </h2>
             <div className="mb-5">
                 {description}
             </div>
+            <button onClick={() => setShowFullDescription((prevState) => !prevState)} className="text-sky-500 margin-bm-5 hover:text-sky-600">{ showFullDescription ? 'Less' : 'More'}</button>
+        </div>
+    )
+  }
 
-            <button onClick={() => setShowFullDescription((prevState) => !prevState)} className="text-indigo-500 margin-bm-5 hover:text-indigo-600">{ showFullDescription ? 'Less' : 'More'}</button>
-    
-            <h3 className="text-indigo-500 mb-2">{job.date}</h3>
-    
-            <div className="border border-gray-100 mb-5"></div>
-    
-            <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
-                    
-                </div>
+  function JobItemB({location, date, alignment = '' }) {
+    return (
+        <div className={`mb-4 ${alignment}`}>
+            <div className="text-gray-600 my-2">
+                {job.location}
             </div>
+            <h3 className="text-sky-500 mb-2">{job.date}</h3>
+        </div>
+    )
+  }
+
+  function JobCard({ job }) {
+    if (job.id % 2 === 0) {
+        return (
+            <>
+                <JobItemA title={job.title} company={job.company} alignment='text-right'/>
+                <JobItemB location={job.location} date={job.date}/>
+            </>
+            )
+    }
+    return (
+        <>
+            <JobItemB location={job.location} date={job.date} alignment='text-right'/>
+            <JobItemA title={job.title} company={job.company}/>
+        </>
+    )
+  }
+
+  return (
+    <div className="bg-white rounded-xl shadow-md relative">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'">
+            <JobCard job={job} />
         </div>
     </div>
   )
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -7,21 +7,18 @@ const Jobs = () => {
 
 
   return (
-    <section className="bg-blue-50 px-4 py-10">
-      <div className="container-xl lg:container m-auto">
+    <section className="bg-sky-700 px-4 py-20">
         <h2 className='text-3xl font-extrabold text-white sm:text-5xl md:text-6xl text-center'>
           Experience
         </h2>
-        
+        <div className="container-xl lg:container m-auto">
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-white">
-            {jobs.reverse().map((job) => (
-              <Job key={job.id} job={ job }/>
-              ))}
-        </div>
-          
-            
-        
+            <div className="grid grid-cols-1 md:grid-cols-1 gap-3 text-white">
+                {jobs.reverse().map((job) => (
+                <Job key={job.id} job={ job }/>
+                ))}
+            </div>
+
       </div>
     </section>
   )
