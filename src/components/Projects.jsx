549786113963b23d15b1ecabf16d7bcc70258444
Tu Nguyen <valcrune@gmail.com>
2024-09-30 11:19:49 -0400

    update project section to include filter row

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -62,28 +62,30 @@ const Projects = () => {
   return (
     <section id= 'projects' className='bg-black-700 px-4 py-20 projects'>
       <h2 className='text-3xl font-extrabold text-white sm:text-5xl md:text-6xl text-center pb-6 pt-3'>
-          Projects
-        </h2>
-        <span className='hr'></span>
+        Projects
+      </h2>
+      <span className='hr'></span>
 
-        {/* <div className="row">
-          <div className="button-group text-center col-lg-12 col-md-12 col-sm-12 text-white px-3">
-            <button type="button" className="active" data-filter="*">All</button>
-            <button type="button" data-filter=".python">Python</button>
-            <button type="button" data-filter=".javascript">Javascript</button>
-            <button type="button" data-filter=".java">Java</button>
-            <button type="button" data-filter=".upcoming">Upcoming</button>
-            <button type="button" data-filter=".other">Other</button>
-          </div>
-        </div> */}
-
-        <div className="container-xl lg:container m-auto">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-white projects-gallery">
-                {projects.map((project) => (
-                <Project key={project.id} project={ project }/>
-                ))}
-            </div>
+      <div className="row">
+        <div className="my-6 text-white pb-2 text-center button-group">
+          <button type="button" className="active" data-filter="*">All</button>
+          <button type="button" data-filter=".python">Python</button>
+          <button type="button" data-filter=".django">Django</button>
+          <button type="button" data-filter=".javascript">Javascript</button>
+          <button type="button" data-filter=".nodejs">NodeJs</button>
+          <button type="button" data-filter=".java">Java</button>
+          <button type="button" data-filter=".upcoming">Upcoming</button>
+          <button type="button" data-filter=".other">Other</button>
         </div>
+      </div>
+
+      <div className="container-xl lg:container m-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-white projects-gallery">
+              {projects.map((project) => (
+              <Project key={project.id} project={ project }/>
+              ))}
+          </div>
+      </div>
 
     </section>
   )
