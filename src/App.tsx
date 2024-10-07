3f9e2a1a3e59b11875374ccd15ab6cb45ac7f5e7
Tu Nguyen <valcrune@gmail.com>
2024-10-07 10:07:10 -0400

    add back route dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import router from "./router";
-import Homepage from "./pages/Homepage";
-import Test from "./pages/Test";
 
-// import V1 from "../pages/V1";
-import V2 from "./pages/V2";
-import Terminal from "./pages/Terminal";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" >
-          <Route index element={<Homepage />} />
-          <Route path="v2" element={<V2 />} />
-          <Route path="terminal" element={<Terminal />} />
-          <Route path="*" element={<Terminal />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
---
+++ src/router/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Homepage from "../pages/Homepage";
+import Test from "../pages/Test";
+import V2 from "../pages/V2";
+import Terminal from "../pages/Terminal";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "/test",
+      element: <Test />,
+    },
+    {
+        path: "/v2",
+        element: <V2 />,
+      },
+      {
+        path: "/terminal",
+        element: <Terminal />,
+      },
+  ]);
+  
+  export default router;
