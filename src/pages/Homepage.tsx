3ea5500bc6b62364df3db6b59cf32b0818546a89
Tu Nguyen <valcrune@gmail.com>
2024-10-09 19:08:37 -0400

    fix terminal-portfolio link

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -33,7 +33,7 @@ function Homepage() {
                             <Link to="/v2"><h4 className="text-white"><strong className="hover:text-sky-600" >click here</strong> to view v2</h4></Link>
                         </div>
                         <div>
-                            <a href="https://tu-nguyen.github.io/terminal"><h4 className="text-sky-600" ><strong className="text-sky-600">click here</strong> to view terminal</h4></a>
+                            <a href="https://tu-nguyen.github.io/terminal-portfolio/"><h4 className="text-sky-600" ><strong className="text-sky-600">click here</strong> to view terminal</h4></a>
                         </div>
                     </div>
                 </div>
